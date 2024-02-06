10901869fe8bac35d2030f27df002bab66fd19db
s3xmaxman <badside3919@gmail.com>
2024-02-06 11:39:03 +0900

    frist commit

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+
+interface CardWrapperProps {
+    children: React.ReactNode;
+    headerLabel: string;
+    backButtonLabel: string;
+    backButtonHref: string;
+    showSocial?: boolean;
+}
+
+
+
+export const CardWrapper = ({ children, headerLabel, backButtonLabel, backButtonHref, showSocial = true }: CardWrapperProps) => {
+    
+}
